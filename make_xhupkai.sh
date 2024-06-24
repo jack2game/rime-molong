11f594119d4dc20da1dfb7aeb4c1959445d72352
jack2game <rex.txt@gmail.com>
2024-06-24 04:23:19 +0200

    maintenance

--- make_xhupkai.sh
+++ make_xhupkai.sh
@@ -61,7 +61,7 @@ python3 ../rime-moran/tools/schemagen.py convert-sp --to=flypy --rime-dict=../xh
 python3 convert_sp.py -i ../xhupkai-cht/zrlf.dict.yaml -o ../xhupkai-cht/zrlf.dict.yaml.bak
 
 sed '/#----------詞庫----------#/q' ../xhupkai-cht/moran_fixed.dict.yaml > ../xhupkai-cht/moran_fixed.dict.yaml.bak
-python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-cht/moran_fixed.dict.yaml > ../xhupkai-cht/temp.txt
+python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-cht/moran_fixed.dict.yaml > ../xhupkai-cht/temp.txt && sed -i '/# Some words for testing\.\.\./,$d' ../xhupkai-cht/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{2,100}\t[A-Za-z]+.*\n//g" ../xhupkai-cht/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{1}\t[A-Za-z]{4}.*\n//g" ../xhupkai-cht/temp.txt
 sed -i '0,/#----------詞庫----------#/d' ../xhupkai-cht/temp.txt  &&  echo "" >> ../xhupkai-cht/moran_fixed.dict.yaml.bak && cat ../xhupkai-cht/temp.txt >> ../xhupkai-cht/moran_fixed.dict.yaml.bak
@@ -75,7 +75,7 @@ opencc -i ../data/assess.tiger-code.com/common.simp.words.txt -o ../xhupkai-cht/
 echo "" >> ../xhupkai-cht/moran_fixed.dict.yaml.bak && cat ../xhupkai-cht/temp.txt >> ../xhupkai-cht/moran_fixed.dict.yaml.bak && perl -i -ne 'print if !$seen{$_}++' ../xhupkai-cht/moran_fixed.dict.yaml.bak
 
 sed '/#----------词库----------#/q' ../xhupkai-cht/moran_fixed_simp.dict.yaml > ../xhupkai-cht/moran_fixed_simp.dict.yaml.bak
-python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-cht/moran_fixed_simp.dict.yaml > ../xhupkai-cht/temp.txt
+python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-cht/moran_fixed_simp.dict.yaml > ../xhupkai-cht/temp.txt && sed -i '/# 化学元素/,$d' ../xhupkai-cht/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{2,100}\t[A-Za-z]+.*\n//g" ../xhupkai-cht/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{1}\t[A-Za-z]{4}.*\n//g" ../xhupkai-cht/temp.txt
 sed -i '0,/#----------词库----------#/d' ../xhupkai-cht/temp.txt  &&  echo "" >> ../xhupkai-cht/moran_fixed_simp.dict.yaml.bak && cat ../xhupkai-cht/temp.txt >> ../xhupkai-cht/moran_fixed_simp.dict.yaml.bak
@@ -125,7 +125,7 @@ python3 ../rime-moran/tools/schemagen.py convert-sp --to=flypy --rime-dict=../xh
 python3 convert_sp.py -i ../xhupkai-chs/zrlf.dict.yaml -o ../xhupkai-chs/zrlf.dict.yaml.bak
 
 sed '/#----------詞庫----------#/q' ../xhupkai-chs/moran_fixed.dict.yaml > ../xhupkai-chs/moran_fixed.dict.yaml.bak
-python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-chs/moran_fixed.dict.yaml > ../xhupkai-chs/temp.txt
+python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-chs/moran_fixed.dict.yaml > ../xhupkai-chs/temp.txt && sed -i '/# Some words for testing\.\.\./,$d' ../xhupkai-chs/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{2,100}\t[A-Za-z]+.*\n//g" ../xhupkai-chs/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{1}\t[A-Za-z]{4}.*\n//g" ../xhupkai-chs/temp.txt
 sed -i '0,/#----------詞庫----------#/d' ../xhupkai-chs/temp.txt  &&  echo "" >> ../xhupkai-chs/moran_fixed.dict.yaml.bak && cat ../xhupkai-chs/temp.txt >> ../xhupkai-chs/moran_fixed.dict.yaml.bak
@@ -139,7 +139,7 @@ opencc -i ../data/assess.tiger-code.com/common.simp.words.txt -o ../xhupkai-chs/
 echo "" >> ../xhupkai-chs/moran_fixed.dict.yaml.bak && cat ../xhupkai-chs/temp.txt >> ../xhupkai-chs/moran_fixed.dict.yaml.bak && perl -i -ne 'print if !$seen{$_}++' ../xhupkai-chs/moran_fixed.dict.yaml.bak
 
 sed '/#----------词库----------#/q' ../xhupkai-chs/moran_fixed_simp.dict.yaml > ../xhupkai-chs/moran_fixed_simp.dict.yaml.bak
-python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-chs/moran_fixed_simp.dict.yaml > ../xhupkai-chs/temp.txt
+python3 ../rime-moran/tools/schemagen.py convert-fixed-sp --to=flypy --rime-dict=../xhupkai-chs/moran_fixed_simp.dict.yaml > ../xhupkai-chs/temp.txt && sed -i '/# 化学元素/,$d' ../xhupkai-chs/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{2,100}\t[A-Za-z]+.*\n//g" ../xhupkai-chs/temp.txt
 perl -CSAD -i -pe "s/^[\x{4e00}-\x{9fff}\x{3007}\x{3400}-\x{4dbf}\x{20000}-\x{2a6df}\x{2a700}-\x{2b73f}\x{2b740}-\x{2b81f}\x{2b820}-\x{2ceaf}\x{2ceb0}-\x{2ebe0}\x{30000}-\x{3134a}\x{31350}-\x{323af}\x{2ebf0}-\x{2ee5f}]{1}\t[A-Za-z]{4}.*\n//g" ../xhupkai-chs/temp.txt
 sed -i '0,/#----------词库----------#/d' ../xhupkai-chs/temp.txt  &&  echo "" >> ../xhupkai-chs/moran_fixed_simp.dict.yaml.bak && cat ../xhupkai-chs/temp.txt >> ../xhupkai-chs/moran_fixed_simp.dict.yaml.bak
